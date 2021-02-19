8b516368522fea462b8cace7c10cacdb2701b588
WMXPY <wm@mengw.io>
2021-02-19 11:00:26 -0600

    add jwt header parsing

--- src/jwt.ts
+++ src/jwt.ts
@@ -4,10 +4,27 @@
  * @description JWT
  */
 
+import { TokenMap } from "./declare";
+import { parseJWTToken } from "./util";
+
 export class JWTToken<Header extends Record<string, any>, Body extends Record<string, any>> {
 
-    public static fromString<Header extends Record<string, any>, Body extends Record<string, any>>(token: string): JWTToken {
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    public static fromString<Header extends Record<string, any>, Body extends Record<string, any>>(token: string): JWTToken<Header, Body> {
 
+        const tokenMap: TokenMap<Header, Body> = parseJWTToken(token);
 
+        return new JWTToken(tokenMap.header, tokenMap.body, tokenMap.signature);
+    }
+
+    private readonly _header: Header;
+    private readonly _body: Body;
+    private readonly _signature: string;
+
+    private constructor(header: Header, body: Body, signature: string) {
+
+        this._header = header;
+        this._body = body;
+        this._signature = signature;
     }
 }
