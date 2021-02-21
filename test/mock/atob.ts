483ad82b5ac9b81ac16112a6e82baa0a93759800
WMXPY <wm@mengw.io>
2021-02-20 21:56:44 -0600

    improve mock function

--- test/mock/atob.ts
+++ test/mock/atob.ts
@@ -6,5 +6,15 @@
  */
 
 export const mockAtobFunction = (value: string): string => {
-    return Buffer.from(value, 'base64').toString('binary');
+
+    const buffer: Buffer = Buffer.from(value, 'base64');
+    const content: string = buffer.toString('utf8');
+    return content;
+};
+
+export const mockBtoaFunction = (value: string): string => {
+
+    const buffer: Buffer = Buffer.from(value);
+    const base64: string = buffer.toString('base64');
+    return base64.replace(/=/g, '');
 };
